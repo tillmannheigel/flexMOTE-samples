cdd4fcebba68385118015f36b323495735de72c7
Christian David <c.david@graphscape.de>
2016-02-29 13:13:54 +0100

    refactored "hello world" demo

--- public/hello-world/js/app.js
+++ public/hello-world/js/app.js
@@ -6,81 +6,41 @@
         throw Error("requires remote.js!");
     }
 
-    var Remote = window.Remote;
+    if (!window.Config) {
+        throw Error("requires config.js");
+    }
 
-    // ----- overrides ---------------------------------------------------------
+    // ----- application -------------------------------------------------------
+    var Remote = window.Remote;
+    var Config = window.Config;
+    var App = window.App = {};
+
     /**
-     * override handleCommand
+     * defines the default handler for remote commands.
      * @param {Object} cmd
      */
-    Remote.handleCommand = function(cmd) {
+    App.handleCommand = function(cmd) {
 
-        // send skin
-        Remote.sendCommand({
-            action: 'set',
-            type: 'skin',
-            id: 'skin-hello-world',
-            data: {
-                url: 'http://localhost:3001/hello-world/skin/style.css'
-            }
-        });
+        switch (cmd.type) {
+            case 'skin':
+            case 'layout':
+            case 'button':
+                Remote.handleCommand(cmd);
+                break;
 
-        // send default layout
-        Remote.sendCommand({
-            action: 'set',
-            type: 'layout',
-            id: 'layout-1',
-            data: {
-                name: 'Layout 1',
-                orientation: 'landscape',
-                cols: 5,
-                rows: 7,
-                elements: [
-
-                // first row
-                {
-                    type: "Button",
-                    id: "home",
-                    action: "home",
-                    label: "HOME"
-                }, {
-                    type: "Text",
-                    id: "title",
-                    content: "Hello World",
-                    cssClass: "blue",
-                    cols: 3
-                }, {
-                    type: "Button",
-                    id: "close",
-                    label: "X",
-                    cssClass: "red"
-                },
-
-                // second row
-                {
-                    type: "HTML",
-                    id: "content",
-                    content: '<p>Hello World!</p><p><strong>Lorem Ipsum</strong> foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat!</p><p>Hello World!</p><p><strong>Lorem Ipsum</strong> foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat!</p><p>Hello World!</p><p><strong>Lorem Ipsum</strong> foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat!</p><p>Hello World!</p><p><strong>Lorem Ipsum</strong> foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat! foo bar bat!</p>',
-                    cols: 5,
-                    rows: 5
-                },
-
-                // third row
-                {
-                    type: "Text",
-                    id: "status",
-                    content: "Status.... (<p>XSS Test</p><script>alert('foo');)",
-                    cols: 5
-                }]
-            }
-        });
+            default:
+                Remote.sendCommand(Config.skins['skin-1'], function() {
+                    Remote.sendCommand(Config.layouts['layout-1']);
+                });
+                break;
+        }
     };
 
-    // ----- initialization  ---------------------------------------------------
+    // ----- initialization ----------------------------------------------------
     // @TODO connect to right channel / room / namespace
     Remote.connection = io('http://localhost:3000');
     Remote.connection.on('connect', Remote.onConnect);
     Remote.connection.on('disconnect', Remote.onDisconnect);
-    Remote.connection.on('cmd', Remote.handleCommand);
+    Remote.connection.on('cmd', App.handleCommand);
 
 })(window);
