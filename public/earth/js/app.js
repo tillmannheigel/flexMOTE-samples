03edbc850454e502ef0e7181a82d2be5fc09d417
Christian David <c.david@graphscape.de>
2016-03-11 08:17:06 +0100

    introducing earth demo

---
+++ public/earth/js/app.js
@@ -0,0 +1,108 @@
+/**
+ * our gui to control the camera
+ */
+var layout = {
+    action: 'set',
+    type: 'layout',
+    id: 'layout-1',
+    data: {
+        name: 'Layout 1',
+        orientation: 'portrait',
+        cols: 3,
+        rows: 3,
+        elements: [{
+        }, {
+            type: "Button",
+            id: "up",
+            action: "up",
+            label: "^"
+        }, {}, {
+            type: "Button",
+            id: "left",
+            action: "left",
+            label: "<"
+        }, {}, {
+            type: "Button",
+            id: "right",
+            action: "right",
+            label: ">"
+        }, {
+        }, {
+            type: "Button",
+            id: "down",
+            action: "down",
+            label: "v"
+        }, {}]
+    }
+};
+
+// ----- flexMOTE --------------------------------------------------------------
+/**
+ * @public socket.io connection
+ */
+flexMOTE.connection = io('http://localhost:3000');
+
+/**
+ * onConnect event handler
+ */
+flexMOTE.connection.on('connect', function() {
+    flexMOTE.DEBUG && console.log('app | onConnect');
+
+    flexMOTE.register({
+        app: 'earth',
+        version: '0.1.0',
+        maxUsers: 1,
+        timeout: 5 * 1000, // 60 seconds
+        stickySessions: false
+    }, function(room) {
+
+        $('body').append('<div id="qrcode" style="text-align:center;position:absolute;top:20px;left:20px;z-index:1000;background:#FFF;padding:20px"></div>');
+        var qrcode = new QRCode("qrcode", {
+            text: "http://localhost:3000/#" + room.toString(),
+            width: 256,
+            height: 256,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+
+        $('#qrcode').append('<p><a target="_blank" style="color:#000;text-decoration:none" href="http://localhost:3000/#' + room + '">localhost:3000<br/>#<strong>' + room + '</strong></a></p>');
+    });
+});
+
+/**
+ *
+ */
+flexMOTE.connection.on('disconnect', function() {
+    $('#qrcode').remove();
+});
+
+/**
+ * onCommand event handler
+ * @param {Object} cmd
+ */
+flexMOTE.connection.on('cmd', function(cmd) {
+    switch (cmd.type) {
+        case 'button':
+            if (cmd.data.state == 'pressed') {
+                switch (cmd.id) {
+                    case 'down':
+                        window.mouse.y += 0.05;
+                        break;
+                    case 'up':
+                        window.mouse.y -= 0.05;
+                        break;
+                    case 'left':
+                        window.mouse.x += 0.05;
+                        break;
+                    case 'right':
+                        window.mouse.x -= 0.05;
+                        break;
+                }
+            }
+
+        case 'user':
+            flexMOTE.sendCommand(cmd.id, layout);
+            break;
+    }
+});
