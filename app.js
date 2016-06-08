d946ae7229b4c8f39b19a245d7345970aa8159be
Tillmann Heigel <heigel@tillmannheigel.de>
2016-06-08 17:08:20 +0200

    added help message and server and port

--- app.js
+++ app.js
@@ -4,15 +4,42 @@ var app = express();
 var http = require('http').Server(app);
 var serveIndex = require('serve-index');
 var compress = require('compression');
+var minimist = require('minimist');
 
-// config
-app.use(compress());
-app.use(express.static(__dirname + '/public'));
-app.use(serveIndex(__dirname + '/public', {
-    'icons': true
-}));
+// check args
+var args = minimist(process.argv.slice(2));
 
-// start
-http.listen(3001, function() {
-    console.log('reMOTE.js - samples; listening on *:3001');
-});
+if(args["h"] || args["help"]) {
+
+    printHelpMessage();
+
+} else {
+
+    //regular server start
+    var server = args["s"] || args ["server"] || "localhost"
+    var port = args["p"] || args ["port"] || "3000"
+    console.log("flextMOTE Server: " + server + ":" + port);
+
+    // config
+    app.use(compress());
+    app.use(express.static(__dirname + '/public'));
+    app.use(serveIndex(__dirname + '/public', {
+        'icons': true
+    }));
+
+    // start
+    http.listen(3001, function() {
+        console.log('flexmote.js - samples; listening on *:3001');
+    });
+
+}
+
+function printHelpMessage(){
+    process.stdout.write(`flexMOTE Usage: node app.js [arguments]
+
+    ARGUMENTS
+    \t-h, --help\t print this help message
+    \t-s, --server\t set the ip address of the flexMOTE server [default: 127.0.0.1]
+    \t-p, --port\t set the port of the flexMOTE server [default: 3000]
+    `+"\n");
+};
